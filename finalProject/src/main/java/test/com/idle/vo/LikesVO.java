d628360b244eb6e3462e18fccd6373994fdee196
ImSoojeong <tnwjd701@gmail.com>
2023-07-17 14:15:02 +0900

    myWrite,myLikes 커밋

--- finalProject/src/main/java/test/com/idle/vo/LikesVO.java
+++ finalProject/src/main/java/test/com/idle/vo/LikesVO.java
@@ -4,9 +4,18 @@ import lombok.Data;
 
 @Data
 public class LikesVO {
-	
 	private int likes_num;
 	private int board_num;
 	private String id;
-
+	
+	//조인으로 추가
+	private int board_status; //1판매중, 2판매완료, 3숨기기
+	private int board_type;  //1팔아요, 2구해요
+	private String board_title;
+	private int price;
+	private String deal_region;
+	private int likecount;
+	private int chatcount;
+	private int view_count;
+	private String board_savename1;
 }
