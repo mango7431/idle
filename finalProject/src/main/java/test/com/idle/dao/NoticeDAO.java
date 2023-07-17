6433f4f6b8c99aa6581a889c6973adcc7a140216
김희강 <kile449@gmail.com>
2023-07-17 11:16:07 +0900

    user

---
+++ finalProject/src/main/java/test/com/idle/dao/NoticeDAO.java
@@ -0,0 +1,18 @@
+package test.com.idle.dao;
+
+import java.util.List;
+
+import test.com.idle.vo.NoticeVO;
+
+public interface NoticeDAO {
+	
+	public List<NoticeVO> selectAll(int cpage);
+	
+	public NoticeVO selectOne(NoticeVO vo);
+
+	public List<NoticeVO> searchList(String searchKey, String searchWord);
+
+	public int noticeRowCount();
+
+
+}
--- finalProject/src/main/java/test/com/idle/vo/BuyVO.java
+++ finalProject/src/main/java/test/com/idle/vo/BuyVO.java
@@ -1,5 +1,9 @@
 package test.com.idle.vo;
 
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Data;
 
 @Data
@@ -11,4 +15,15 @@ public class BuyVO {
 	
 	private String id;
 	
+	private String board_savename1;
+	
+	private List<MultipartFile> multipartFiles;
+	
+	private String board_title;
+	
+	private int view_count;
+	
+	private String deal_region;
+	
+	private int price;
 }
