e1937d7aef27b7a00f36d611c6ef8bf4bf069460
김애리 <zjadofl0408@gmail.com>
2023-07-20 09:04:05 +0900

    Feat: 관리자 - Q&A 답변시 알림 전송 기능 구현
    - 알림의 양에 따라 뱃지 표시
    - 기존 브라우저 알림을 토스트 알림으로 변경

---
+++ finalAdmin/src/main/java/test/com/admin/repository/EmitterRepository.java
@@ -0,0 +1,21 @@
+package test.com.admin.repository;
+
+import java.util.Map;
+
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+public interface EmitterRepository {
+	public SseEmitter save(String emitterId, SseEmitter emitter);
+	
+	public void saveEventCache(String emitterId, Object event);
+	
+	Map<String, SseEmitter> findAllEmitterStartWithById(String memberId);
+	
+    Map<String, Object> findAllEventCacheStartWithById(String memberId);
+	
+	public void deleteById(String id);
+	
+	public void deleteAllEmitterStartWithId(String memberId);
+	
+	public void deleteAllEventCacheStartWithId(String memberId);
+}
